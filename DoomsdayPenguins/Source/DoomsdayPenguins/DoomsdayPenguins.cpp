251ef2a9dc28c1b27f997e364dbf50459cc99345
Tmmrvg <Oetom01@outlook.com>
2023-04-17 15:01:31 +0200

    Unreal Upload

---
+++ DoomsdayPenguins/Source/DoomsdayPenguins/DoomsdayPenguins.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DoomsdayPenguins.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DoomsdayPenguins, "DoomsdayPenguins" );
