9f2a52f32b3eddc26bbb586920f4ca62157e1662
Eileenov <eileen_o_v@yahoo.no>
2023-04-27 14:36:54 +0200

    Level update 2.0
    værsåsnill hva skjer her

---
+++ DoomsdayPenguins/Source/DoomsdayPenguins/HUD_Doomsday_Penguins.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "HUD_Doomsday_Penguins.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DOOMSDAYPENGUINS_API AHUD_Doomsday_Penguins : public AHUD
+{
+	GENERATED_BODY()
+	
+};
